6682a5b6cb38f52f342e8baa07053cb514cc7fdd
Almaz's Zefirka <almazbek.sheraliev321@gmail.com>
2025-04-15 14:21:31 +0600

    init

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+document.querySelector("#app")!.innerHTML = `
+  <div>
+    <a href="/about">About</a> |
+    <a href="/blog/article">Article</a>
+  </div>
+`;
+
+document.querySelector("#counter");
---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { sync } from "glob";
+import { ViteMinifyPlugin } from "vite-plugin-minify";
+
+export default {
+  root: "./src",
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: sync("./src/*.html".replace(/\\/g, "/")),
+    },
+  },
+
+  plugins: [ViteMinifyPlugin()],
+};
